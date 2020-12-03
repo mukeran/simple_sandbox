003d4692ba36316153278714716a445b87ca3fd3
mukeran <mukeran@mukeran.com>
2020-12-03 14:28:29 +0800

    :tada: Integration without ML finished.

--- simple_sandbox.py
+++ simple_sandbox.py
@@ -4,27 +4,16 @@ import filters, decoder
 
 def start():
   logging.basicConfig(level=logging.DEBUG)
-  # sniffer = engines.HTTPSniffer('lo0')
+  logging.info('Starting...')
   watcher = engines.FileWatcher(['./test'])
-  # logging.debug("Start to register decoder...")
-  # sniffer.register_decoder(decoder.UrlDecoder)
-  # sniffer.register_decoder(decoder.Base64Decoder)
-  # logging.debug("Start to register filters...")
-  # sniffer.register_filter(filters.SqliFilter())
-  # sniffer.register_filter(filters.XSSFilter())
-  # sniffer.register_filter(filters.WebshellFilter())
-  # sniffer.register_filter(filters.BinaryFilter())
-  # watcher.register_filter(filters.WebshellFilter())
   watcher.register_filter(filters.BinaryFilter())
+  fpm_sniffer = engines.FPMSniffer()
 
-  # logging.info("Initialized")
-  # sniffer.start()
   watcher.start()
-  # sniffer.join()
+  fpm_sniffer.start()
+  logging.info("Initialized")
   watcher.join()
-  # fpm_sniffer = engines.FPMSniffer()
-  # fpm_sniffer.start()
-  # fpm_sniffer.join()
+  fpm_sniffer.join()
 
 
 if __name__ == '__main__':
